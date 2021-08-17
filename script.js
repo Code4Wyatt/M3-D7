7d5b414009d77bced5298c23d30e75c11fe8cbea
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-08-17 15:34:16 +0100

    Search not working correctly

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
-window.onload = () => {
-    fetchUsers();
-}
-    
+let names = []
 
+window.onload = () => {
+    fetchUsers()
+}
 
 async function fetchUsers() {
     try {
         let response = await fetch('https://jsonplaceholder.typicode.com/users')
         let data = await response.json()
         // wait until the request completes
-        console.log(data);
+        console.log(data)
     } catch {
         console.log(err)
     }
+}
+
+
+
+function search(query) {
+    async function fetchUsers() {
+    try {
+        let response = await fetch('https://jsonplaceholder.typicode.com/users')
+        let data = await response.json()
+        // wait until the request completes
+        console.log(data)
+         if (query.length < 3) {
+        filteredNames = names;
+        filteredNames();
+        return;
+    }
+     filteredNames = users.filter((user) => {
+        user.name.toLowerCase().includes(query, toLowerCase())
+    });
+    } catch {
+        console.log(err)
+    }
+}
+    
+    
+    
 }
