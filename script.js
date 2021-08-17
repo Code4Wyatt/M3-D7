e4aa2459a8ce30e447d839199c721087dc8ccd7b
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-08-17 13:58:20 +0100

    Retrieved data to console from API

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-script.js
+window.onload = () => {
+    fetchUsers();
+}
+    
+
+
+async function fetchUsers() {
+    try {
+        let response = await fetch('https://jsonplaceholder.typicode.com/users')
+        let data = await response.json()
+        // wait until the request completes
+        console.log(data);
+    } catch {
+        console.log(err)
+    }
+}
